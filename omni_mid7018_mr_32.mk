1c504a1d6d01fc733a0d608f82d7086651fd4afd
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-04-09 23:00:33 +0530

    Update omni_mid7018_mr_32.mk

--- omni_mid7018_mr_32.mk
+++ omni_mid7018_mr_32.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from mid7018_mr_32 device
 $(call inherit-product, device/onn/mid7018_mr_32/device.mk)
